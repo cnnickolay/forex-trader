100e22af2fd9ae3b62fb588ba55e3dbd12a7c161
Nikolai Cherkezishvili <nickolayc@gmail.com>
2017-07-13 14:12:02 +0100

    added tests for CMO

--- instruments/src/test/scala/org/nikosoft/oanda/instruments/Oscillators$Test.scala
+++ instruments/src/test/scala/org/nikosoft/oanda/instruments/Oscillators$Test.scala
@@ -6,6 +6,28 @@ import scalaz.Scalaz._
 
 class Oscillators$Test extends FunSpec with Matchers {
 
+  describe("CMO") {
+    it("should calculate cmo at the beginning") {
+      val input: Seq[BigDecimal] = Seq(18235, 17980, 18310, 18605, 18980, 18915, 19475, 19050, 19065, 18850, 19025, 19005, 18505, 18635, 19245, 19305, 19125, 19525, 19775, 20385, 20515)
+      val expected: BigDecimal = -40.7143
+      val actual = CMO.cmo(21, input)
+      actual.fold(fail("this should return something")) { _ shouldBe (expected +- 1e-4) }
+    }
+
+    it("should calculate cmo when there is more values than required by the period") {
+      val input: Seq[BigDecimal] = Seq(17845, 18235, 17980, 18310, 18605, 18980, 18915, 19475, 19050, 19065, 18850, 19025, 19005, 18505, 18635, 19245, 19305, 19125, 19525, 19775, 20385, 20515)
+      val expected: BigDecimal = -43.3447
+      val actual = CMO.cmo(21, input)
+      actual.fold(fail("this should return something")) { _ shouldBe (expected +- 1e-4) }
+    }
+
+    it("should return None if period is greater than amount of values") {
+      val input: Seq[BigDecimal] = Seq(1, 2)
+      CMO.cmo(3, input) shouldBe None
+      CMO.cmo(2, input) shouldNot be(None)
+    }
+  }
+  
   describe("rsi") {
     it("should calculate rsi for array of values") {
       val input: Seq[BigDecimal] = Seq(46.28, 46.28, 45.61, 46.03, 45.89, 46.08, 45.84, 45.42, 45.10, 44.83, 44.33, 43.61, 44.15, 44.09, 44.34)
