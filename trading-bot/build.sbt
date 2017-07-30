aaddafeebe60b731b1991b5e0fa0e457715445f9
cnnickolay <nickolayc@gmail.com>
2017-07-30 11:35:45 +0100

    trying simple deduplicator with streams

--- trading-bot/build.sbt
+++ trading-bot/build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.12.2"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" % "akka-actor_2.12" % "2.5.3",
-  "com.typesafe.akka" % "akka-stream_2.12" % "2.5.3"
+  "com.typesafe.akka" % "akka-stream_2.12" % "2.5.3",
+  "com.typesafe.akka" % "akka-stream-contrib_2.12" % "0.8"
 )
 
--- trading-bot/src/main/scala/org/nikosoft/oanda/bot/streaming/Dedup.scala
+++ trading-bot/src/main/scala/org/nikosoft/oanda/bot/streaming/Dedup.scala
@@ -1,27 +1,33 @@
 package org.nikosoft.oanda.bot.streaming
 
-import akka.NotUsed
-import akka.actor.{ActorSystem, Cancellable}
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape, FlowShape}
 import akka.stream.scaladsl._
-import akka.stream.{ActorMaterializer, ClosedShape}
-
-import scala.concurrent.duration.DurationInt
 
 object Dedup extends App {
 
   implicit val actorSystem = ActorSystem("streamer")
   implicit val materializer = ActorMaterializer()
 
-  val source = Source(List(1, 2, 2, 2, 2, 2, 3, 4, 4, 5, 6))
+  val source = Source(List(1, 1, 1, 2, 2, 2, 2, 2, 3, 4, 4, 5, 6))
 
-  val flow = Flow[Int]
-    .sliding(2, 1)
-    .mapConcat { case prev +: next +: _ => if (prev == next) Nil else List(next) }
+  RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
 
+    val index = Source(Stream.from(1))
+    val zip = builder.add(Zip[Int, Int]())
+    index ~> zip.in0
+    source ~> zip.in1
 
-  source
-    .via(flow)
-    .runWith(Sink.foreach(println))
+    zip.out ~> Flow[(Int, Int)]
+      .sliding(2, 1)
+      .mapConcat { case (prevIdx, prevValue) +: (idx, value) +: _ =>
+        if (idx == 1) List(prevValue, value)
+        else if (prevValue == value) Nil
+        else List(prevValue)
+      } ~> Sink.foreach(println)
 
+    ClosedShape
+  }).run()
 
 }
