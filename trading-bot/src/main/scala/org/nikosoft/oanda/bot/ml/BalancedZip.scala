0753a3b0a1142a2a7e5bcbe7fcafb9cbf9149bae
Nikolai Cherkezishvili <nickolayc@gmail.com>
2017-08-17 16:06:20 +0100

    implemented BalancedZip

--- trading-bot/src/main/scala/org/nikosoft/oanda/bot/ml/BalancedZip.scala
+++ trading-bot/src/main/scala/org/nikosoft/oanda/bot/ml/BalancedZip.scala
@@ -1,10 +1,11 @@
 package org.nikosoft.oanda.bot.ml
 
+import java.time.LocalDateTime
+
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
-import scalaz.Scalaz._
 
 class BalancedZip[T](nextEnrichCondition: (T, T) => Boolean) extends GraphStage[FanInShape2[T, T, (T, T)]] {
 
@@ -19,19 +20,25 @@ class BalancedZip[T](nextEnrichCondition: (T, T) => Boolean) extends GraphStage[
     var nextEnrichOption: Option[T] = None
     var lastIncomingOption: Option[T] = None
 
+    var enrichFinished = false
+
     setHandler(in0, new InHandler {
       def onPush(): Unit = {
-        lastIncomingOption = Option(grab(in0))
-        ^(lastIncomingOption, lastEnrichOption) {(incoming, lastEnrich) =>
-          nextEnrichOption match {
-            case Some(nextEnrich) =>
-              lastEnrichOption = nextEnrichOption
-              nextEnrichOption = None
-              nextEnrichCondition(incoming, nextEnrich)
-            case None =>
-          }
-          push(out, (incoming, lastEnrich))
+        val lastIncoming = grab(in0)
+        lastIncomingOption = Option(lastIncoming)
+
+        val enrichOption: Option[T] = (lastEnrichOption, nextEnrichOption) match {
+          case (Some(_lastEnrich), Some(nextEnrich)) if nextEnrichCondition(lastIncoming, nextEnrich) =>
+            lastEnrichOption = nextEnrichOption
+            nextEnrichOption = None
+            Option(nextEnrich)
+          case (Some(_lastEnrich), _) => Option(_lastEnrich)
+          case (None, _) => None
         }
+        enrichOption.fold(println("nothing to push"))(enrich => {
+          val elem = (lastIncoming, enrich)
+          push(out, elem)
+        })
       }
     })
 
@@ -46,11 +53,14 @@ class BalancedZip[T](nextEnrichCondition: (T, T) => Boolean) extends GraphStage[
           case (None, None) => lastEnrichOption = grabbedValue
         }
       }
+
+      @throws[Exception](classOf[Exception])
+      override def onUpstreamFinish(): Unit = {}
     })
 
     setHandler(out, new OutHandler {
       def onPull(): Unit = {
-        if (nextEnrichOption.isEmpty) pull(in1)
+        if (nextEnrichOption.isEmpty && !hasBeenPulled(in1)) tryPull(in1)
         pull(in0)
       }
     })
@@ -62,17 +72,18 @@ object Main extends App {
   implicit val actorSystem = ActorSystem("test")
   implicit val actorMaterializer = ActorMaterializer()
 
-  val source1 = Source(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-  val source2 = Source(List(1, 5, 7, 10))
+  val start = LocalDateTime.of(2010, 1, 1, 0, 0, 0)
+  val source1 = Source((0 until 30).map(start.plusDays(_)))
+  val source2 = Source((0 to 20 by 5).map(start.plusDays(_)))
 
   RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
 
-    val zip = builder.add(new BalancedZip[Int]((main, enrich) => main > enrich))
+    val zip = builder.add(new BalancedZip[LocalDateTime]((main, enrich) => main.isAfter(enrich) || main.isEqual(enrich)))
 
     source1 ~> zip.in0
     source2 ~> zip.in1
-               zip.out ~> Sink.foreach[(Int, Int)](println)
+    zip.out ~> Sink.foreach(println)
 
     ClosedShape
   }).run()
